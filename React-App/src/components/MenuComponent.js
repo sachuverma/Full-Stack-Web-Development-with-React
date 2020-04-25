3158a5e8b406bdd0b2f35f74ece12d3d181ea8d7
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-04-25 22:52:22 +0530

    add selected dish component

--- React-App/src/components/MenuComponent.js
+++ React-App/src/components/MenuComponent.js
@@ -1,12 +1,6 @@
+import DishDetail from "./DishDetailComponent";
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
@@ -27,22 +21,6 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle heading>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
@@ -61,7 +39,8 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+
+        <DishDetail selectedDish={this.state.selectedDish} />
       </div>
     );
   }
