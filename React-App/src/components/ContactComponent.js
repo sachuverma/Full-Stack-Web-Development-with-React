25b9694f589c3b8a61c553b320937fa54c9cc041
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-04 22:37:12 +0530

    redux form validation functions

--- React-App/src/components/ContactComponent.js
+++ React-App/src/components/ContactComponent.js
@@ -12,6 +12,13 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component {
   constructor(props) {
     super(props);
