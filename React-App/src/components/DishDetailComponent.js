055304b460cc4ff1b9ebb19681a63c02cedf1201
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 13:33:59 +0530

    modified DishDetail

--- React-App/src/components/DishDetailComponent.js
+++ React-App/src/components/DishDetailComponent.js
@@ -17,6 +17,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -183,7 +184,23 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 const DishDetails = (props) => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMessage}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
