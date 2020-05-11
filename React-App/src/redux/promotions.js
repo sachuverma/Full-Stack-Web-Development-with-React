69f4f64f0af2c7ebfa18428277916e8460527946
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 21:15:33 +0530

    update redux/ comment and promo js

--- React-App/src/redux/promotions.js
+++ React-App/src/redux/promotions.js
@@ -1,7 +1,38 @@
-import { PROMOTIONS } from "../shared/promotions";
+import * as ActionTypes from "./ActionTypes";
 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (
+  state = {
+    isLoading: true,
+    errMessage: null,
+    promos: [],
+  },
+  action
+) => {
   switch (action.type) {
+    case ActionTypes.ADD_PROMOS:
+      return {
+        ...state,
+        isLoading: false,
+        errMessage: null,
+        promos: action.payload,
+      };
+
+    case ActionTypes.PROMOS_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        errMessage: null,
+        promos: [],
+      };
+
+    case ActionTypes.PROMOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMessage: action.payload,
+        promos: [],
+      };
+
     default:
       return state;
   }
