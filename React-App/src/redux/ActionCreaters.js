a05484d197c9211437b5e20224aff381ff992b1b
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 21:08:42 +0530

    update action creators

--- React-App/src/redux/ActionCreaters.js
+++ React-App/src/redux/ActionCreaters.js
@@ -15,9 +15,9 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
 
-  setTimeout(() => {
-    dispatch(addDishes(DISHES));
-  }, 2000);
+  return fetch(baseUrl + "dishes")
+    .then((response) => response.json())
+    .then((dishes) => dispatch(addDishes(dishes)));
 };
 
 export const dishesLoading = () => ({
@@ -32,4 +32,42 @@ export const dishesFailed = (err) => ({
 export const addDishes = (dishes) => ({
   type: ActionTypes.ADD_DISHES,
   payload: dishes,
+});
+
+export const fetchComments = () => (dispatch) => {
+  return fetch(baseUrl + "comments")
+    .then((response) => response.json())
+    .then((comments) => dispatch(addComments(comments)));
+};
+
+export const commentsFailed = (err) => ({
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: err,
+});
+
+export const addComments = (comments) => ({
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments,
+});
+
+export const fetchPromos = () => (dispatch) => {
+  dispatch(promosLoading(true));
+
+  return fetch(baseUrl + "promotions")
+    .then((response) => response.json())
+    .then((promos) => dispatch(addPromos(promos)));
+};
+
+export const promosLoading = () => ({
+  type: ActionTypes.PROMOS_LOADING,
+});
+
+export const promosFailed = (err) => ({
+  type: ActionTypes.PROMOS_FAILED,
+  payload: err,
+});
+
+export const addPromos = (promos) => ({
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos,
 });
