0c643aba2d680f00e2219bed4fbd5beb9ff5f883
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 14:08:16 +0530

    modified Menu Component

--- React-App/src/components/LoadingComponent.js
+++ React-App/src/components/LoadingComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Loading = () => {
   return (
     <div className="col-12">
-      <span className="fa fa-spinner fa-pulse fa-3x fa-fw test-primary"></span>
+      <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
       <p>Loading...</p>
     </div>
   );
