17b58f6f6cf0221d1bbc0a99d13960a6725f5239
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 19:39:03 +0530

    add cross fetch module

--- React-App/src/components/LoadingComponent.js
+++ React-App/src/components/LoadingComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Loading = () => {
   return (
-    <div className="col-12 mt-2 mb-2">
+    <div className="col-12 mt-5 mb-5">
       <span className="fa fa-spinner fa-pulse fa-4x fa-fw text-primary"></span>
       <span className="text-secondary display-4">Loading...</span>
     </div>
