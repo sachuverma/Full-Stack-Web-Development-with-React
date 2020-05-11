3815fc9557eb60bc55681d947b2cfcd03cfbee5e
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-11 14:13:20 +0530

    REDUX THUNK

--- React-App/src/components/LoadingComponent.js
+++ React-App/src/components/LoadingComponent.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Loading = () => {
   return (
-    <div className="col-12">
-      <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
-      <p>Loading...</p>
+    <div className="col-12 mt-2 mb-2">
+      <span className="fa fa-spinner fa-pulse fa-4x fa-fw text-primary"></span>
+      <span className="text-secondary display-4">Loading...</span>
     </div>
   );
 };
