8578be6db67954b2d535873721af330a513fd51b
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-12 22:21:53 +0530

    add build from react to json server to serve all files at same port

---
+++ conFusion-ReactappDB-server/public/precache-manifest.970037645ed3e0d5776af83cf1da17d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960745280cdd23f8545e146884a402fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16613aeaaf2fdb5258c3",
+    "url": "/static/css/2.7d9277a0.chunk.css"
+  },
+  {
+    "revision": "9f79899b8161ef400800",
+    "url": "/static/css/main.8e674221.chunk.css"
+  },
+  {
+    "revision": "16613aeaaf2fdb5258c3",
+    "url": "/static/js/2.867b8d5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.867b8d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f79899b8161ef400800",
+    "url": "/static/js/main.698d04a5.chunk.js"
+  },
+  {
+    "revision": "564a8b4ebcdf34bb9c6a",
+    "url": "/static/js/runtime-main.b4491e00.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
