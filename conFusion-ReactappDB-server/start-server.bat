d1ff4c1051369f9399607e5637c487430e2de0e0
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-12 22:29:30 +0530

    update bat file

--- conFusion-ReactappDB-server/start-server.bat
+++ conFusion-ReactappDB-server/start-server.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
 Title conFusion JSON-Server
-ECHO Port should be between 0 and 65536
-SET /P port="Enter PORT to start server on: "
+:: ECHO Port should be between 0 and 65536
+:: SET /P port="Enter PORT to start server on: "
 ECHO.
-ECHO Starting conFusion JSON-Server on PORT: %port%
-ECHO (use ctrl+c to stop server)	
+ECHO Starting conFusion JSON-Server on PORT: 3001
+ECHO (use ctrl+c to stop server)
+ECHO starting server at http://localhost:3001/
+START "" http://localhost:3001/
+
 ECHO ==============================================
-json-server --watch db.json -p %port% -d 2000
+json-server --watch db.json -p 3001 -d 2000
 PAUSE
