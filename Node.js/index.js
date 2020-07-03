f0791a4914afff1890c2479258071323a1806489
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-07-03 23:49:53 +0530

    http server completed

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,12 +1,41 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const hostname = 'localhost';
 const port = 69;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>Hello!</h1></body></html>');
+  console.log("Request for "+ req.url + " bu method "+ req.method);
+  if(req.method == 'GET'){
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    var filePath = path.resolve('./public'+fileUrl);
+    const fileExt = path.extname(filePath);
+    if(fileExt == '.html'){
+      fs.exists(filePath, (exists) => {
+        if(!exists) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<html><body>Error 404: '+ fileUrl +' not found</body></html>');
+        } else {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          fs.createReadStream(filePath).pipe(res);
+        }
+      })
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body>Error 404: '+ fileUrl +' not an html file</body></html>');
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body>Error 404: req method not supported</body></html>');
+  }
 });
 
 server.listen(port,hostname, ()=>{
