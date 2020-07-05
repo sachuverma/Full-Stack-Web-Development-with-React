fdd84e07925a1f96f5d778ce0bd0644d889da84b
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-07-05 22:03:51 +0530

    express rest

--- Node.js/express/index.js
+++ Node.js/express/index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
   http = require('http'),
+  bodyParser = require('body-parser'),
   morgan = require('morgan');
 
 const hostname = 'localhost';
@@ -9,6 +10,28 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode=200;
+  res.setHeader('Content-Type','text/plain');
+  next();
+});
+
+app.get('/dishes', (req, res, next) => {
+  res.end('Will send all the dishes to you!');
+});
+
+app.post((req, res, next) => {
+  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+app.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+})
+app.delete((req, res, next) => {
+  res.end('Deleting all dishes');
+});
 
 app.use((req, res, next) => {
   res.statusCode = 200;
