23db822faf6162b11f0e4654f6c45768919b1006
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-07-05 22:07:21 +0530

    express rest

--- Node.js/express/index.js
+++ Node.js/express/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
   http = require('http'),
   bodyParser = require('body-parser'),
   morgan = require('morgan');
+const { report } = require('process');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -22,15 +23,30 @@ app.get('/dishes', (req, res, next) => {
   res.end('Will send all the dishes to you!');
 });
 
-app.post((req, res, next) => {
+app.post('/dishes', (req, res, next) => {
   res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 })
-app.put((req, res, next) => {
+app.put('/dishes', (req, res, next) => {
   res.statusCode = 403;
   res.end('PUT operation not supported on /dishes');
 })
-app.delete((req, res, next) => {
+app.delete('/dishes', (req, res, next) => {
   res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+  res.end('Will send the detail of /dishes/' + req.params.dishId);
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.end('POST not supported on /dishes/'+ req.params.dishId);
+})
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('Update dish /dishes/'+req.params.dishId);
+})
+app.delete('/dishes/:dishId', (req, res, next) => {
+  res.end('Deleting dishes/'+req.params.dishId);
 });
 
 app.use((req, res, next) => {
